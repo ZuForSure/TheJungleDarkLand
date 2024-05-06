1cbc1236914451a04d3d49991131b563337b2f25
ZuForSure <anhvux20se@gmail.com>
2024-05-06 09:19:05 +0700

    Initial commit

---
+++ Assets/_Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+    public static InputManager Instance => instance;
+
+    protected float inputHorizontal;
+    protected float inputVertical;
+    protected float inputDash;
+
+    private void Awake()
+    {
+        InputManager.instance = this;
+    }
+
+    public virtual float GetInputHorizontal()
+    {
+        this.inputHorizontal = Input.GetAxisRaw("Horizontal");
+        return this.inputHorizontal;
+    }
+
+    public virtual float GetInputVertical()
+    {
+        this.inputVertical = Input.GetAxisRaw("Vertical");
+        return this.inputVertical;
+    }
+
+    public virtual float GetInputDash()
+    {
+        this.inputDash = Input.GetAxisRaw("Jump");
+        return this.inputDash;
+    }
+
+
+}
